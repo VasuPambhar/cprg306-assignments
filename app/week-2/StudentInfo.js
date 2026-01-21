0778dd6840fbe4a610055237a4814f0946b6cfe9
VasuPambhar <136171266+VasuPambhar@users.noreply.github.com>
2026-01-21 16:30:27 -0700

    Initialize Project

---
+++ app/week-2/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return(
+    <main>
+      <section>
+        <h1>Name: Vasu Pambhar</h1>
+        <Link href="#">Github Link</Link>
+      </section>
+    </main>
+  );
+}
