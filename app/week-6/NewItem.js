ef003854624604db970c36dd7f6c76437c0a351d
VasuPambhar <136171266+VasuPambhar@users.noreply.github.com>
2026-02-23 21:50:53 -0700

    Week 6 added

--- app/week-6/NewItem.js
+++ app/week-6/NewItem.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 
-export default function NewItem() {
+export default function NewItem({ onAddItem }) {
   const [name, setName] = useState("");
   const [quantity, setQuantity] = useState(1);
   const [category, setCategory] = useState("produce");
@@ -9,12 +9,13 @@ export default function NewItem() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const item = {
+      id : crypto.randomUUID(),
       name : name,
       quantity : quantity,
       category : category
     };
     console.log(item);
-    alert(`Added: ${item.name}, quantity: ${item.quantity}, category: ${item.category}`);
+    onAddItem(item);
     setName("");
     setQuantity(1);
     setCategory("produce");
@@ -23,12 +24,12 @@ export default function NewItem() {
   return(
     <form className="max-w-md mx-auto bg-white px-7 py-10 rounded-2xl" onSubmit={handleSubmit}>
       <label htmlFor="item-name" className="dark:text-gray-900">Name:</label>
-      <input className="w-full p-2 rounded text-center border-2 border-gray-700 dark:text-gray-900" id="item-name" type="text" value={name} onChange={(event) => setName(event.target.value)} required />
+      <input className="w-full p-2 rounded text-center border-2 border-gray-700 dark:text-gray-900" id="item-name" name="item-name" type="text" value={name} onChange={(event) => setName(event.target.value)} required />
       <div className="flex flex-col gap-4 my-4">
         <label htmlFor="quantity" className="dark:text-gray-900">Quantity:</label>
-        <input className="text-center border-2 border-gray-700 rounded p-2 dark:text-gray-900" id="quantity" type="number" min={1} max={99} value={quantity} onChange={(event) => setQuantity(Number(event.target.value))} />
+        <input className="text-center border-2 border-gray-700 rounded p-2 dark:text-gray-900" id="quantity" name="quantity" type="number" min={1} max={99} value={quantity} onChange={(event) => setQuantity(Number(event.target.value))} />
         <label htmlFor="category" className="dark:text-gray-900">Category:</label>
-        <select className="text-center border-2 border-gray-700 rounded p-2 dark:text-gray-900" id="category" value={category} onChange={(event) => setCategory(event.target.value)}>
+        <select className="text-center border-2 border-gray-700 rounded p-2 dark:text-gray-900" id="category" name="category" value={category} onChange={(event) => setCategory(event.target.value)}>
           <option value="produce">Produce</option>
           <option value="dairy">Dairy</option>
           <option value="bakery">Bakery</option>
---
+++ app/week-6/page.js
@@ -0,0 +1,26 @@
+"use client";
+import ItemList from "./item-list";
+import data from "./items.json";
+import { useState } from "react";
+import NewItem from "./NewItem";
+
+
+export default function Page() {
+  const [items, setItems] = useState(data);
+
+  const handleAddItem = (newItem) => {
+    setItems([...items,newItem]);
+  };
+
+  return (
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gray-900 font-bold">
+        <header className="w-full p-10">
+          <h1 className="text-white text-3xl font-bold text-center">Shopping List</h1>
+        </header>
+        <section>
+          <NewItem onAddItem={handleAddItem}/>
+          <ItemList item={items} />
+        </section>
+      </main>
+  );
+}
